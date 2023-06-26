46ec74c1c1523a6fc134054562b986d8a08d9469
arrrriskandar <arrrriskandar@gmail.com>
2023-06-26 15:17:04 +0800

    completed

---
+++ script.js
@@ -0,0 +1,22 @@
+var phrases = ["Football ", "Traveler ", "Adventurer ", "Cyrophile " , "Foodie "];
+var prePhrases = ["a ", "an avid ", "an ", "a ", "a "];
+var postPhrases = [" fan ", " ", " ", " ", " "];
+var emojis = ["&#x26BD;", "&#x2708;", "&#x1F3D4;","&#x2744;", "&#x1F374;"];
+var currentIndex = 0;
+
+function changePhrase() {
+  var outputElement = document.getElementById("outputSentence");
+  var phrase = phrases[currentIndex];
+  var prePhrase = prePhrases[currentIndex];
+  var postPhrase = postPhrases[currentIndex];
+  var emoji = emojis[currentIndex];
+  var sentence = "I'm an aspiring " + "<strong>Software Engineer </strong>" + "and " + prePhrase + " <strong>" + phrase + "</strong>" + '<span class="emoji">' + emoji + '</span>' + postPhrase;
+
+  outputElement.innerHTML = sentence;
+  currentIndex = (currentIndex + 1) % phrases.length;
+  }
+setInterval(changePhrase, 3000);
+
+function handleFormSubmit() {
+  alert("Form submitted successfully!");
+}
